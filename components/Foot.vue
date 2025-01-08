12dc4a402dd3145a5e0b51c6872565c642d1e358
Jordan <jordan@driedsponge.net>
2025-01-07 18:29:47 -0800

    update footer year

--- components/Foot.vue
+++ components/Foot.vue
@@ -1,29 +1,34 @@
 <template>
-	<footer class="border-t-2 border-t-base-300 bg-base-100 py-8 mt-5">
+	<footer class="mt-5 border-t-2 border-t-base-300 bg-base-100 py-8">
 		<div
-			class="max-w-6xl container mx-auto flex flex-col content-center items-center justify-between space-y-6 px-3 align-middle lg:flex-row lg:space-y-0 xl:px-0"
+			class="container mx-auto flex max-w-6xl flex-col content-center items-center justify-between space-y-6 px-3 align-middle lg:flex-row lg:space-y-0 xl:px-0"
 		>
 			<div class="text-center text-gray-300 lg:text-left">
 				<ul>
 					<li>
-						GitHub: <a
-						class="underline"
-						target="_blank"
-						href="https://github.com/jortuck">jortuck</a
-					>
+						GitHub:
+						<a
+							class="underline"
+							target="_blank"
+							href="https://github.com/jortuck"
+							>jortuck</a
+						>
 					</li>
 					<li>
-						LinkedIn: <a
-						class="underline"
-						target="_blank"
-						href="https://linkedin.com/in/jortuck">jortuck</a
-					>
+						LinkedIn:
+						<a
+							class="underline"
+							target="_blank"
+							href="https://linkedin.com/in/jortuck"
+							>jortuck</a
+						>
 					</li>
 					<li>
 						<a
 							class="underline"
 							target="_blank"
-							href="https://github.com/jortuck/driedsponge.net">Website Source Code</a
+							href="https://github.com/jortuck/driedsponge.net"
+							>Website Source Code</a
 						>
 					</li>
 				</ul>
@@ -32,10 +37,9 @@
 				<p class="text-center text-3xl font-extrabold text-white lg:text-right">
 					<span class="text-primary">J</span>T
 				</p>
-				<p class="text-xs text-gray-400">© 2024 Jordan Tucker</p>
+				<p class="text-xs text-gray-400">© 2025 Jordan Tucker</p>
 			</div>
 		</div>
 	</footer>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
